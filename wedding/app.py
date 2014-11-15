225add403becebb3031cc5c4e0d974a2327b81ae
Luke Lovett <luke.lovett@mongodb.com>
2014-11-15 13:12:42 -0800

    Add MongoDB connection. Add other skeletal code.

--- wedding/app.py
+++ wedding/app.py
@@ -1,8 +1,28 @@
 import os
+
 from bottle import route, run
+from pymongo import MongoClient
+
+
+MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
+MONGO_PORT = int(os.environ.get('MONGO_PORT', 27017))
+
+db = MongoClient(MONGO_HOST, MONGO_PORT).wedding
+
+
+@route('/photo/upload', method='POST')
+def upload_photo():
+    pass
+
 
 @route('/')
 def hello():
-        return "Hello World!"
+    return "Hello World!"
 
-run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+def main():
+    run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=True)
+
+
+if __name__ == '__main__':
+    main()
