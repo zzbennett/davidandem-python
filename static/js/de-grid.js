3b08dfb338d32d82f89632f907ee89c7a220d743
Luke Lovett <luke.lovett@mongodb.com>
2014-11-23 11:43:06 -0800

    untabify everything

--- static/js/de-grid.js
+++ static/js/de-grid.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
     // rows/columns of images.
     var imageGrid = [
-	[
-	    {on: '/static/images/photos/denver.png',
-	     off: '/static/images/photos/modified/denver-sepia.png',
-	     page: "travel-information"},
-	    {on: '/static/images/photos/b&w smiling.jpg',
-	     off: '/static/images/photos/modified/b&w smiling sepia.jpg',
-	     page: "about"},
-	    {on: '/static/images/photos/cute and smiling in front of structure.jpg',
-	     off: '/static/images/photos/modified/cute and smiling in front of structure sepia.jpg',
-	     page: "wedding-day"},
-	    {on: '/static/images/photos/DAC.png',
-	     off: '/static/images/photos/modified/DAC-sepia.png',
-	     page: "wedding-day"}
-	],
-	[
-	    {on: '/static/images/photos/emillie is dipped.jpg',
-	     off: '/static/images/photos/modified/emillie is dipped sepia.jpg'},
-	    {on: '/static/images/photos/Ampersand.png',
-	     off: '/static/images/photos/modified/Ampersand-sepia.png',
-	     page: "registry"},
-	    {on: '/static/images/photos/emillie looks blissful.jpg',
-	     off: '/static/images/photos/modified/emillie looks blissful sepia.jpg',
-	     page: "RSVP"},
-	    {on: '/static/images/photos/kiss.jpg',
-	     off: '/static/images/photos/modified/kiss sepia.jpg',
-	     page: "photos"}
-	]
+        [
+            {on: '/static/images/photos/denver.png',
+             off: '/static/images/photos/modified/denver-sepia.png',
+             page: "travel-information"},
+            {on: '/static/images/photos/b&w smiling.jpg',
+             off: '/static/images/photos/modified/b&w smiling sepia.jpg',
+             page: "about"},
+            {on: '/static/images/photos/cute and smiling in front of structure.jpg',
+             off: '/static/images/photos/modified/cute and smiling in front of structure sepia.jpg',
+             page: "wedding-day"},
+            {on: '/static/images/photos/DAC.png',
+             off: '/static/images/photos/modified/DAC-sepia.png',
+             page: "wedding-day"}
+        ],
+        [
+            {on: '/static/images/photos/emillie is dipped.jpg',
+             off: '/static/images/photos/modified/emillie is dipped sepia.jpg'},
+            {on: '/static/images/photos/Ampersand.png',
+             off: '/static/images/photos/modified/Ampersand-sepia.png',
+             page: "registry"},
+            {on: '/static/images/photos/emillie looks blissful.jpg',
+             off: '/static/images/photos/modified/emillie looks blissful sepia.jpg',
+             page: "RSVP"},
+            {on: '/static/images/photos/kiss.jpg',
+             off: '/static/images/photos/modified/kiss sepia.jpg',
+             page: "photos"}
+        ]
     ];
 
     // Load Handlebars template.
@@ -37,125 +37,125 @@ $(document).ready(function () {
     var parentDiv = $("#grid-wrap");
 
     for (var row=0; row<imageGrid.length; row++) {
-	for (var col=0; col<imageGrid[row].length; col++) {
-	    var imageObj = imageGrid[row][col];
-	    var capitalize = function(s) {
-		return s.charAt(0).toUpperCase() + s.slice(1);
-	    };
-	    if (imageObj.page) {
-		var title = imageObj.page.split("-").map(capitalize).join(" ");
-	    } else {
-		var title = null;
-	    }
-	    var gridDiv = $(template({
-		row: row,
-		col: col,
-		imageObj: imageObj,
-		pageTitle: title
-	    }));
-	    // Hover handlers
-	    var titleSlideDuration = 400;
-	    gridDiv.siblings('.grid-on').hover(
-		function() {
-		    // Fade out the "on" image.
-		    $(this).stop().fadeTo(200, 1.0);
-		    // Get the title associated with the on image.
-		    var myTitle = $(this).children(".grid-title");
-		    // Get the title associated with the off image.
-		    var offTitle = $(".grid-off-" + $(this).data("rollover-id"))
-			.children(".grid-title");
-		    if (myTitle.hasClass("grid-title-row-0")) {
-			// If we're on the top row...
-			// Record where the original top/left coordinates were.
-			if (! myTitle.data("orig-left")) {
-			    myTitle.data({
-				"orig-left": myTitle.css("left"),
-				"orig-top": myTitle.css("top")
-			    });
-			}
-			// Move both on and off titles to new location.
-			myTitle.animate({left: "30px", top: "30px"}, {duration: titleSlideDuration});
-			offTitle.animate({left: "30px", top: "30px"}, {duration: titleSlideDuration});
-		    } else if (myTitle.hasClass("grid-title-row-1")) {
-			// If we're on the bottom row...
-			// Record where the original bottom/right coordinates were.
-			if (! myTitle.data("orig-right")) {
-			    myTitle.data({
-				"orig-right": myTitle.css("right"),
-				"orig-bottom": myTitle.css("bottom")
-			    });
-			}
-			// Move both on and off title sto new location.
-			myTitle.animate({right: "30px", bottom: "30px"}, {duration: titleSlideDuration});
-			offTitle.animate({right: "30px", bottom: "30px"}, {duration: titleSlideDuration});
-		    }
-		},
-		function() {
-		    // Fade out the on image.
-		    $(this).stop().fadeTo(2000, 0.0);
-		    // Get the title associated with the on image.
-		    var onTitle = $(this).children(".grid-title");
-		    // Get the title associated with the off image.
-		    var offDiv = $(".grid-off-" + $(this).data("rollover-id"));
-		    // Get its title.
-		    var myTitle = offDiv.children(".grid-title");
-		    if (myTitle.hasClass("grid-title-row-0")) {
-			// If we're on the top row...
-			// Get the original top/left coordinates.
-			var leftDest = onTitle.data("orig-left");
-			var topDest = onTitle.data("orig-top");
-			// Move the titles back to their original location.
-			myTitle.animate({left: leftDest, top: topDest}, {duration: titleSlideDuration});
-			onTitle.animate({left: leftDest, top: topDest}, {duration: titleSlideDuration});
-		    } else if (myTitle.hasClass("grid-title-row-1")) {
-			// If we're on the bottom row...
-			// Get the original bottom/right coordinates.
-			var rightDest = onTitle.data("orig-right");
-			var bottomDest = onTitle.data("orig-bottom");
-			// Move the titles back to their original position.
-			myTitle.animate({right: rightDest, bottom: bottomDest}, {duration: titleSlideDuration});
-			onTitle.animate({right: rightDest, bottom: bottomDest}, {duration: titleSlideDuration});
-		    }
-		}
-	    ).click(function(event) {
-		var page = $(this).data("page");
-		openModal(page);
-		event.stopPropagation();
-	    });
-	    parentDiv.append(gridDiv);
-	}
+        for (var col=0; col<imageGrid[row].length; col++) {
+            var imageObj = imageGrid[row][col];
+            var capitalize = function(s) {
+                return s.charAt(0).toUpperCase() + s.slice(1);
+            };
+            if (imageObj.page) {
+                var title = imageObj.page.split("-").map(capitalize).join(" ");
+            } else {
+                var title = null;
+            }
+            var gridDiv = $(template({
+                row: row,
+                col: col,
+                imageObj: imageObj,
+                pageTitle: title
+            }));
+            // Hover handlers
+            var titleSlideDuration = 400;
+            gridDiv.siblings('.grid-on').hover(
+                function() {
+                    // Fade out the "on" image.
+                    $(this).stop().fadeTo(200, 1.0);
+                    // Get the title associated with the on image.
+                    var myTitle = $(this).children(".grid-title");
+                    // Get the title associated with the off image.
+                    var offTitle = $(".grid-off-" + $(this).data("rollover-id"))
+                        .children(".grid-title");
+                    if (myTitle.hasClass("grid-title-row-0")) {
+                        // If we're on the top row...
+                        // Record where the original top/left coordinates were.
+                        if (! myTitle.data("orig-left")) {
+                            myTitle.data({
+                                "orig-left": myTitle.css("left"),
+                                "orig-top": myTitle.css("top")
+                            });
+                        }
+                        // Move both on and off titles to new location.
+                        myTitle.animate({left: "30px", top: "30px"}, {duration: titleSlideDuration});
+                        offTitle.animate({left: "30px", top: "30px"}, {duration: titleSlideDuration});
+                    } else if (myTitle.hasClass("grid-title-row-1")) {
+                        // If we're on the bottom row...
+                        // Record where the original bottom/right coordinates were.
+                        if (! myTitle.data("orig-right")) {
+                            myTitle.data({
+                                "orig-right": myTitle.css("right"),
+                                "orig-bottom": myTitle.css("bottom")
+                            });
+                        }
+                        // Move both on and off title sto new location.
+                        myTitle.animate({right: "30px", bottom: "30px"}, {duration: titleSlideDuration});
+                        offTitle.animate({right: "30px", bottom: "30px"}, {duration: titleSlideDuration});
+                    }
+                },
+                function() {
+                    // Fade out the on image.
+                    $(this).stop().fadeTo(2000, 0.0);
+                    // Get the title associated with the on image.
+                    var onTitle = $(this).children(".grid-title");
+                    // Get the title associated with the off image.
+                    var offDiv = $(".grid-off-" + $(this).data("rollover-id"));
+                    // Get its title.
+                    var myTitle = offDiv.children(".grid-title");
+                    if (myTitle.hasClass("grid-title-row-0")) {
+                        // If we're on the top row...
+                        // Get the original top/left coordinates.
+                        var leftDest = onTitle.data("orig-left");
+                        var topDest = onTitle.data("orig-top");
+                        // Move the titles back to their original location.
+                        myTitle.animate({left: leftDest, top: topDest}, {duration: titleSlideDuration});
+                        onTitle.animate({left: leftDest, top: topDest}, {duration: titleSlideDuration});
+                    } else if (myTitle.hasClass("grid-title-row-1")) {
+                        // If we're on the bottom row...
+                        // Get the original bottom/right coordinates.
+                        var rightDest = onTitle.data("orig-right");
+                        var bottomDest = onTitle.data("orig-bottom");
+                        // Move the titles back to their original position.
+                        myTitle.animate({right: rightDest, bottom: bottomDest}, {duration: titleSlideDuration});
+                        onTitle.animate({right: rightDest, bottom: bottomDest}, {duration: titleSlideDuration});
+                    }
+                }
+            ).click(function(event) {
+                var page = $(this).data("page");
+                openModal(page);
+                event.stopPropagation();
+            });
+            parentDiv.append(gridDiv);
+        }
     }
 
     var closeModals = function(modals) {
-	modals.stop().animate(
-	    {opacity: 0.0},
-	    {
-		duration: 300,
-		complete: function() {
-		    $(this).css("z-index", 0);
-		}
-	    }
-	);
+        modals.stop().animate(
+            {opacity: 0.0},
+            {
+                duration: 300,
+                complete: function() {
+                    $(this).css("z-index", 0);
+                }
+            }
+        );
     };
 
     var allModals = $(".modal");
     var closeAllModals = function() { closeModals( allModals ); };
     var openModal = function(page) {
-	closeAllModals();
-	// Don't open another modal if there are already some open.
-	var modalsOpen = allModals.filter(
-	    function() { return parseFloat($(this).css("opacity")) > 0.0; }
-	).length > 0;
-	if (! modalsOpen) {
-	    $("#" + page)
-		.stop()
-		.animate({opacity: 1.0}, 200)
-		.css("z-index", 101);
-	}
+        closeAllModals();
+        // Don't open another modal if there are already some open.
+        var modalsOpen = allModals.filter(
+            function() { return parseFloat($(this).css("opacity")) > 0.0; }
+        ).length > 0;
+        if (! modalsOpen) {
+            $("#" + page)
+                .stop()
+                .animate({opacity: 1.0}, 200)
+                .css("z-index", 101);
+        }
     };
 
     $(".modal-close").click(function() {
-	closeModals( $(this).parents(".modal") );
+        closeModals( $(this).parents(".modal") );
     });
 
     $("body").click( closeAllModals );
--- templates/grid.tpl
+++ templates/grid.tpl
@@ -11,22 +11,22 @@
       <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min.js"></script>
 {% raw %}
       <script type="text/x-handlebars-template" id="rollover-template">
-	<div class="grid-image grid-off grid-off-{{ row }}-{{ col }} row{{ row }} col{{ col }}"
-	     data-rollover-id="{{ row }}-{{ col }}"
-	     {{#if imageObj.page }}data-page="{{ imageObj.page }}"{{/if}}
-	     style="background-image: url('{{ imageObj.off }}'); background-position: {{ imageObj.position.x }}px {{ imageObj.position.y }}px;">
-	     {{#if pageTitle }}
-	     <h2 class="grid-title grid-title-row-{{ row }}">{{ pageTitle }}</h2>
-	     {{/if}}
-	</div>
-	<div class="grid-image grid-on grid-on-{{ row }}-{{ col }} row{{ row }} col{{ col }}"
-	     data-rollover-id="{{ row }}-{{ col }}"
-	     {{#if imageObj.page }}data-page="{{ imageObj.page }}"{{/if}}
-	     style="background-image: url('{{ imageObj.on }}'); background-position: {{ imageObj.position.x }}px {{ imageObj.position.y }}px; opacity: 0.0;">
-	     {{#if pageTitle }}
-	     <h2 class="grid-title grid-title-row-{{ row }}">{{ pageTitle }}</h2>
-	     {{/if}}
-	</div>
+        <div class="grid-image grid-off grid-off-{{ row }}-{{ col }} row{{ row }} col{{ col }}"
+             data-rollover-id="{{ row }}-{{ col }}"
+             {{#if imageObj.page }}data-page="{{ imageObj.page }}"{{/if}}
+             style="background-image: url('{{ imageObj.off }}'); background-position: {{ imageObj.position.x }}px {{ imageObj.position.y }}px;">
+             {{#if pageTitle }}
+             <h2 class="grid-title grid-title-row-{{ row }}">{{ pageTitle }}</h2>
+             {{/if}}
+        </div>
+        <div class="grid-image grid-on grid-on-{{ row }}-{{ col }} row{{ row }} col{{ col }}"
+             data-rollover-id="{{ row }}-{{ col }}"
+             {{#if imageObj.page }}data-page="{{ imageObj.page }}"{{/if}}
+             style="background-image: url('{{ imageObj.on }}'); background-position: {{ imageObj.position.x }}px {{ imageObj.position.y }}px; opacity: 0.0;">
+             {{#if pageTitle }}
+             <h2 class="grid-title grid-title-row-{{ row }}">{{ pageTitle }}</h2>
+             {{/if}}
+        </div>
       </script>
 {% endraw %}
       <script type="text/javascript" src="/static/js/de-grid.js"> </script>
@@ -39,15 +39,15 @@
     <!-- travel info -->
     <div id="travel-information" class="modal">
       <div class="modal-header">
-	<span class="modal-title">Travel Information</span>
-	<span class="modal-close"><a href="#">&times;</a></span>
+        <span class="modal-title">Travel Information</span>
+        <span class="modal-close"><a href="#">&times;</a></span>
       </div>
       <div class="modal-body">
-	<p class="modal-text">The wedding will be held at the Denver Athletic Club, in the heart of Denver, CO! There are ample hotels from which to choose in the area. See the map below for the location of the athletic club and surrounding accommodations.</p>
-	<h2>Map</h2>
-	<!-- copied from google maps, search for "hotels near denver athletic club" -->
-	<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3067.9083735405816!2d-104.99369000000003!3d39.741709000000014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1shotels+near+denver+athletic+club!5e0!3m2!1sen!2sus!4v1416094351807"
-		class="map" frameborder="0"></iframe>
+        <p class="modal-text">The wedding will be held at the Denver Athletic Club, in the heart of Denver, CO! There are ample hotels from which to choose in the area. See the map below for the location of the athletic club and surrounding accommodations.</p>
+        <h2>Map</h2>
+        <!-- copied from google maps, search for "hotels near denver athletic club" -->
+        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3067.9083735405816!2d-104.99369000000003!3d39.741709000000014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1shotels+near+denver+athletic+club!5e0!3m2!1sen!2sus!4v1416094351807"
+                class="map" frameborder="0"></iframe>
       </div>
     </div>
 
@@ -55,14 +55,14 @@
     <!-- photos -->
     <div id="photos" class="modal">
       <div class="modal-header">
-	<span class="modal-title">Photos</span>
-	<span class="modal-close"><a href="#">&times;</a></span>
+        <span class="modal-title">Photos</span>
+        <span class="modal-close"><a href="#">&times;</a></span>
       </div>
       <div class="modal-body">
-	<p class="modal-text">Here are some photos of our favorite most adorable couple!</p>
-	<h2>They Look Like Movie Stars</h2>
-	<!-- copied from google maps, search for "hotels near denver athletic club" -->
-	<img src="http://shechive.files.wordpress.com/2012/02/a-kitty-cat-7.jpg" class="photo-image" />
+        <p class="modal-text">Here are some photos of our favorite most adorable couple!</p>
+        <h2>They Look Like Movie Stars</h2>
+        <!-- copied from google maps, search for "hotels near denver athletic club" -->
+        <img src="http://shechive.files.wordpress.com/2012/02/a-kitty-cat-7.jpg" class="photo-image" />
       </div>
     </div>
 
@@ -70,18 +70,18 @@
     <!-- wedding day -->
     <div id="wedding-day" class="modal">
       <div class="modal-header">
-	<span class="modal-title">Wedding Day</span>
-	<span class="modal-close"><a href="#">&times;</a></span>
+        <span class="modal-title">Wedding Day</span>
+        <span class="modal-close"><a href="#">&times;</a></span>
       </div>
       <div class="modal-body">
-	<p class="modal-text">That's what we came here for, isn't it?</p>
-	<h2>Useful Information</h2>
-	<h3>Schedule of the day</h3>
-	<p>Do stuff. More stuff. Drink. Eat. Dance. Talk.</p>
-	<h3>Where stuff is happening</h3>
-	<p>It's all over the place. Wear your walking shoes. Bring your gym pants.</p>
-	<h3>What to bring</h3>
-	<p>Remember that no flash photography is allowed, so as not to upset the bride and groom.</p>
+        <p class="modal-text">That's what we came here for, isn't it?</p>
+        <h2>Useful Information</h2>
+        <h3>Schedule of the day</h3>
+        <p>Do stuff. More stuff. Drink. Eat. Dance. Talk.</p>
+        <h3>Where stuff is happening</h3>
+        <p>It's all over the place. Wear your walking shoes. Bring your gym pants.</p>
+        <h3>What to bring</h3>
+        <p>Remember that no flash photography is allowed, so as not to upset the bride and groom.</p>
       </div>
     </div>
 
@@ -89,13 +89,13 @@
     <!-- rsvp -->
     <div id="RSVP" class="modal">
       <div class="modal-header">
-	<span class="modal-title">RSVP</span>
-	<span class="modal-close"><a href="#">&times;</a></span>
+        <span class="modal-title">RSVP</span>
+        <span class="modal-close"><a href="#">&times;</a></span>
       </div>
       <div class="modal-body">
-	<p class="modal-text">This is going to be <em>the</em> wedding of Spring 2015, and tickets are already selling out! Get yours now!</p>
-	<h2>Limit of 2 Tickets Per Customer</h2>
-	<img src="http://i270.photobucket.com/albums/jj84/addiewest/gettinheldup.jpg" />
+        <p class="modal-text">This is going to be <em>the</em> wedding of Spring 2015, and tickets are already selling out! Get yours now!</p>
+        <h2>Limit of 2 Tickets Per Customer</h2>
+        <img src="http://i270.photobucket.com/albums/jj84/addiewest/gettinheldup.jpg" />
       </div>
     </div>
 
@@ -103,13 +103,13 @@
     <!-- rsvp -->
     <div id="registry" class="modal">
       <div class="modal-header">
-	<span class="modal-title">Registry</span>
-	<span class="modal-close"><a href="#">&times;</a></span>
+        <span class="modal-title">Registry</span>
+        <span class="modal-close"><a href="#">&times;</a></span>
       </div>
       <div class="modal-body">
-	<p class="modal-text">You gotta pay if you wanna see the love.</p>
-	<h2>Limit of 2 Tickets Per Customer</h2>
-	<img src="http://dailykitten.com/wp-content/uploads/2007/03/1173796830boris-bela-3.jpg" />
+        <p class="modal-text">You gotta pay if you wanna see the love.</p>
+        <h2>Limit of 2 Tickets Per Customer</h2>
+        <img src="http://dailykitten.com/wp-content/uploads/2007/03/1173796830boris-bela-3.jpg" />
       </div>
     </div>
 
@@ -117,18 +117,18 @@
     <!-- about -->
     <div id="about" class="modal">
       <div class="modal-header">
-	<span class="modal-title">About</span>
-	<span class="modal-close"><a href="#">&times;</a></span>
+        <span class="modal-title">About</span>
+        <span class="modal-close"><a href="#">&times;</a></span>
       </div>
       <div class="modal-body">
-	<p class="modal-text">Learn some stuff about people important to the wedding.</p>
-	<h2>David Bennet</h2>
-	<p>David is witty, wise, and whimsical.</p>
-	<img src="http://lookingtobusiness.com/wp-content/uploads/2010/01/Cat-reading-a-book.jpg" />
-	<h2>Emillie Lastname</h2>
-	<p>Emillie is giggly, gregarious, and good-humored.</p>
-	<h2>Fido Bennett</h2>
-	<p>Fido is resentful, ruthless, and ravenous.</p>
+        <p class="modal-text">Learn some stuff about people important to the wedding.</p>
+        <h2>David Bennet</h2>
+        <p>David is witty, wise, and whimsical.</p>
+        <img src="http://lookingtobusiness.com/wp-content/uploads/2010/01/Cat-reading-a-book.jpg" />
+        <h2>Emillie Lastname</h2>
+        <p>Emillie is giggly, gregarious, and good-humored.</p>
+        <h2>Fido Bennett</h2>
+        <p>Fido is resentful, ruthless, and ravenous.</p>
       </div>
     </div>
 
@@ -140,15 +140,15 @@
     <div id="center-medallion">
 
       <div id="date-div">
-	  <h1>6.12.2015</h1>
-	  <img src="/static/images/underline.png"></img>
+          <h1>6.12.2015</h1>
+          <img src="/static/images/underline.png"></img>
       </div>
 
       <div id="david" class="medallian-name">
-	  <h1>David</h1>
+          <h1>David</h1>
       </div>
       <div id="emillie" class="medallian-name">
-	  <h1>Emillie</h1>
+          <h1>Emillie</h1>
       </div>
     </div>
   </body>
